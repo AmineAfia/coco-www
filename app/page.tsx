e52153bf83f5c2da9801732b65f3451b797b117e
Amine Afia <amine@thirdweb.com>
2025-09-30 17:55:06 +0200

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/hero";
 import Features from "@/components/features";
 import { Navbar } from "@/components/navbar";
+import Footer from '@/components/footer';
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
         {/* <FAQ /> */}
         {/* <Testimonials /> */}
         {/* <CTABanner /> */}
-        {/* <Footer /> */}
+        <Footer />
       </main>
     </>
   );
